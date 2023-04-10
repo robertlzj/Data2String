07e53cae3eb47337a223c062f8616988c5d29635
RobertL <robertlzj@users.noreply.github.com>
2023-04-10 16:24:07 +0800

    enhance
    *optimise for speed
    +Bench / Performance
    +support NaN/ math number
    +support function
    +handle Keywords
    +enhance compress

---
+++ Bench.lua
@@ -0,0 +1,63 @@
+--	modified from [serpent](https://github.com/pkulchenko/serpent/blob/master/t/bench.lua).
+
+local socket=require'socket'
+local get_time=socket.gettime
+	and os.clock
+
+local ITERS = 1000
+
+---------- test data  ---------
+local b = {text="ha'ns", ['co\nl or']='bl"ue', str="\"\n'\\\001"}
+local a = {
+  x=1, y=2, z=3,
+  ['function'] = b, -- keyword as a key
+  list={'a',nil,nil, -- shared reference, embedded nils
+        [9]='i','f',[5]='g',[7]={}}, -- empty table
+  ['label 2'] = b, -- shared reference
+  [math.huge] = -math.huge, -- huge as number value
+}
+a.c = a -- self-reference
+
+---------- serialize  ----------
+local Serializer = {
+	serpent = function(...) return require("serpent").dump(...) end,
+	penlight = function(...) return require("pl.pretty").write(...) end,
+	D2S = function(...) return require'Data2String2'(...) end,
+	D2S_compress = function(...) return require'Data2String2'(...,'compress') end,
+	'serpent','penlight','D2S','D2S_compress',
+}
+
+for index, serializer_name in ipairs(Serializer) do
+	--load require
+	local serializer=Serializer[serializer_name]
+	serializer()
+end
+
+for index, serializer_name in ipairs(Serializer) do
+	local serializer=Serializer[serializer_name]
+	local start, str = get_time()
+	for _ = 1, ITERS do str = serializer(a) end
+	print(("serializer %s (%d): %6.4fs"):format(serializer_name, ITERS, get_time() - start))
+	collectgarbage'collect';collectgarbage'collect'
+end
+
+--------- deserialize  ---------
+local Deserializers = {
+  serpent = load,
+	D2S = load,
+	D2S_compress = load,
+	'serpent','D2S','D2S_compress',
+}
+
+for index, Serializer_Name in ipairs(Deserializers) do
+	local Serializer=Serializer[Serializer_Name]
+	local str = Serializer(a)
+	local copy
+	local start = get_time()
+	local Deserializer=Deserializers[Serializer_Name]
+  for _ = 1, ITERS do
+		copy=Deserializer(str)()
+	end
+  print(("deserializer %s (%d): %6.4fs"):format(Serializer_Name, ITERS, get_time() - start))
+	collectgarbage'collect';collectgarbage'collect'
+end
--- Data2String.lua
+++ Data2String.lua
@@ -1,45 +1,77 @@
 --Data2String2.lua by RobertL
-local T,P,IP,I,F,String_Format,Concat_Table,Repeat_String,next=type,pairs,ipairs,table.insert,math.floor,string.format,table.concat,string.rep,next
-local function Wrap_String(String)
+local Get_Type,P,IP,I,TR,F,String_Format,Concat_Table,Repeat_String,Next,String_Length,String_Dump,String_Sub,String_Find=type,pairs,ipairs,table.insert,table.remove,math.floor,string.format,table.concat,string.rep,next,string.len,string.dump,string.sub,string.find
+
+local Keywords={}
+for _,Keyword in ipairs({'and', 'break', 'do', 'else', 'elseif', 'end', 'false',
+  'for', 'function', 'goto', 'if', 'in', 'local', 'nil', 'not', 'or', 'repeat',
+  'return', 'then', 'true', 'until', 'while'}) do
+	Keywords[Keyword] = true
+end
+
+local function N() end
+
+local Is_Normal,Is_Compress=true,false
+--	Is_Normal: won't compress
+--	Is_Compress: compressed, a little faster
+
+local function String_Converter(String)
 	local String_Wrapped_By_Format_Q=String_Format('%q',String)
 	--	[[\"...\"]]
-	local Converted_String
-	if string.sub(String_Wrapped_By_Format_Q,2,-2)~=String then
+	local Prefix,Converted_String,Postfix
+	if Is_Normal and String_Sub(String_Wrapped_By_Format_Q,2,-2)~=String then
 		;	local Literal_String_Level=0
 		;	local Literal_String_Start,Literal_String_End
 		repeat
-			Literal_String_Start='['..string.rep('=',Literal_String_Level)..'['
-			Literal_String_End=']'..string.rep('=',Literal_String_Level)..']'
+			Literal_String_Start='['..Repeat_String('=',Literal_String_Level)..'['
+			Literal_String_End=']'..Repeat_String('=',Literal_String_Level)..']'
 			Literal_String_Level=Literal_String_Level+1
-		until not string.find(String,Literal_String_Start,1,true) and not string.find(String,Literal_String_End,1,true)
-		Converted_String=Literal_String_Start..String..Literal_String_End
+		until not String_Find(String,Literal_String_Start,1,true) and not String_Find(String,Literal_String_End,1,true)
+		Prefix=Literal_String_Start
+		Converted_String=String
+		Postfix=Literal_String_End
 	else
 		Converted_String=String_Wrapped_By_Format_Q
 	end
-	return Converted_String
+	return Converted_String,Prefix,Postfix
+end--String_Converter
+
+local Input_Converter={
+	[math.huge]='1/0',[-math.huge]='-1/0',
+	[true]='true',[false]='false',
+}
+if math.mininteger then
+	Input_Converter[math.mininteger]='math.maxinteger'
+	Input_Converter[math.mininteger]='math.mininteger'
 end
+
 local function Data2String(Data,Configure)
 	--[[
 		Configure: default true
 			true
-			false, for read only (can't load) result after 'return'
+			'compress', could `load`, no format
+			false, format for read only (can't load)
 				equal {C=false}
 				will failed, if self-reference exist
 			table={P=pairs,Paris=pairs,String_Converter=Wrap_String,SC=Wrap_String,Reference_String_If_Longer_Than_Length=10,RS=10}
 	]]
-	local Pairs,String_Converter,CO=P,Wrap_String,','--pairs,string convert,comma
+	local Pairs,String_Converter,Comma=P,String_Converter,','--pairs,string convert,comma
 	local RS=10
 	--	Reference_String_If_Longer_Than_Length
 	local SO
 	--	scan only, use internally?
 	
+	Is_Normal,Is_Compress=true,false
+
 	if Configure==nil then
 		Configure=true
+	elseif Configure=='compress' then
+		Is_Normal=false
+		Is_Compress=true
 	else
-		if T(Configure)=='table' then
+		if Get_Type(Configure)=='table' then
 			Pairs=Configure.P or Configure.Pairs or Pairs
 			String_Converter=Configure.CS or Configure.String_Converter or String_Converter
-			CO=Configure.CO or CO
+			Comma=Configure.CO or Comma
 			SO=Configure.SO
 			Configure.RS=Configure.RS or Configure.Reference_String_If_Longer_Than_Length
 			Configure.C=Configure.C or Configure.Configure
@@ -51,52 +83,55 @@ local function Data2String(Data,Configure)
 			if Configure.C==false then
 				Configure=nil
 			end
-		else
+		else assert(Configure==false,'unhandle Configure')
 			SO=false
-			CO=''
+			Comma=''
 			String_Converter=assert
 			Configure=nil
 		end
 	end
-	local ts={--[[tables
-		[t]=false/1/2,--reference
-		--then
-		[id]=true/flase,--could refer (or define at first time)
+	local Objects={--[[Objects
+		[Object]=Object_Reference_Count,
+		--	'nil' or 'number'.
 	]]}
-	local t
-	local function Scan(input)
-		t=T(input)
-		if t=='table' or (t=='string' and RS and input:len()>RS) then
-			ts[input]=(ts[input] or 0)+1
-			if ts[input]>1 then return end
-			if t=='table' then for k,v in Pairs(input) do
-				Scan(k);Scan(v)
-			end end
+	local Type
+	local function Scan(Input)
+		Type=Get_Type(Input)
+		if Type=='table' or (Is_Normal and Type=='string' and RS and String_Length(Input)>RS) then
+			if Objects[Input] then
+				Objects[Input]=Objects[Input]+1
+				assert(Configure or type(Input)~='table',"can't use `false` for configure since there is circle-reference")
+			else
+				Objects[Input]=1
+				if Type=='table' then for k,v in Pairs(Input) do
+					Scan(k);Scan(v)
+				end end
+			end
 		end
 	end
 	Scan(Data)
 	if SO then
-		return ts
+		return Objects
 	end
 	
-	--generate id, clean single instance
-	local id=1
+	--generate ID, clean single instance
+	local ID=1
 	local tc={--[[
 		[id]=count
 	]]}
-	for t,c in P(ts) do
+	for t,c in P(Objects) do
 		if c>1 then
-			ts[t]=id
-			tc[id]=c
-			id=id+1
+			Objects[t]=ID
+			tc[ID]=c
+			ID=ID+1
 		else
-			ts[t]=nil
+			Objects[t]=nil
 		end
 	end
 
 	local o={--output list
 		'--Generated using Data2String2.lua by RobertL\n',
-		Configure and next(ts) and [[local _,Func=setmetatable({},{
+		Configure and Next(Objects) and [[local _,Func=setmetatable({},{
 	__index=function(R,id) R[id]={} return R[id] end,
 	__call=function(R,id,t)
 		if rawget(R,id) and assert(type(t)=='table') then
@@ -111,90 +146,111 @@ local function Data2String(Data,Configure)
 }),error
 return ]] or 'return '}
 	local LE--append on line end (comment of index)
-	local Need_Break_Sequent_Open_Bracket
-	local function W(text)
-		text=text or '\n'
-		if text=='\n' and LE then
-			text=LE..text
+	local Write_Newline=Is_Compress and N or function()
+		if LE then
+			I(o,LE)
 			LE=nil
 		end
-		I(o,text)
-		Need_Break_Sequent_Open_Bracket=false
+		return I(o,'\n')
 	end
-	local function indent(Level)
-		W(Repeat_String('\t',Level))
+	local function Write(text)
+		return I(o,text)
 	end
-	local function R(i,n)--reference(input,config,indent number)
-		t=T(i)
-		local id=Configure and t~='number' and ts[i]
-		if id then
-			W'_'
-			if ts[id] then
-				W'['W(id)W']'
+	
+	local Indent=Is_Compress and N or function(Level)
+		return Write(Repeat_String('\t',Level))
+	end
+	local Need_Break_Sequent_Open_Bracket
+	local function Reference(Input,Indent_Level)
+		Type=Get_Type(Input)
+		local ID=Configure and Type~='number' and Objects[Input]
+		if ID then
+			Write'_'
+			if Objects[ID] then
+				Write'['Write(ID)Write']'
 				return
 			else
-				W'('W(id)W','
-				ts[id]=id
+				Write'('Write(ID)Write','
+				Objects[ID]=ID
 			end
 		end
-		if t=='string' then
-			local Wrapped_String=String_Converter(i)
-			if Need_Break_Sequent_Open_Bracket and string.sub(Wrapped_String,1,1)=='[' then
-				W' '--break `[[..]]` to `[ [..]]`.
+		local Converted_Input=Input_Converter[Input]
+		if Converted_Input then
+			Write(Converted_Input)
+		elseif Type=='string' then
+			local Converted_String,Prefix,Postfix=String_Converter(Input)
+			if Need_Break_Sequent_Open_Bracket then
+				Need_Break_Sequent_Open_Bracket=false
+				if o[#o]=='[' and String_Sub(Prefix or Converted_String,1,1)=='[' then
+					Write' '
+				end
 			end
-			W(Wrapped_String)
-		elseif t=='number' then
-			if i==F(i) then
-				i=F(i)--convert to integer
+			if Prefix then
+				Write(Prefix)
 			end
-			W(i)
-		elseif t=='boolean' then
-			W(i and 'true' or 'false')
-		elseif t=='nil' then
-			W'nil'
-		elseif t=='function' then
-			W'Func'
-		elseif t=='table' then
-			W'{'
-			if next(i)==nil then
-				W'}'
+			Write(Converted_String)
+			if Postfix then
+				Write(Postfix)
+			end
+		elseif Type=='number' then
+			if Input~=Input--[[NaN eg `0/0` ]] then
+				Converted_Input='0/0'
+			elseif Input==F(Input) then
+				Converted_Input=F(Input)
+			end
+			Write(Converted_Input or Input)
+		elseif Type=='nil' then
+			Write'nil'
+		elseif Type=='function' then
+			Write(String_Dump(Input))
+		elseif Type=='table' then
+			Write'{'
+			if Next(Input)==nil then
+				Write'}'
 			else
 				local is={}
-				for k,v in Pairs(i) do--simulate ipairs
-					if T(k)=='number' then
+				for k,v in Pairs(Input) do--simulate ipairs
+					if Get_Type(k)=='number' then
 						is[k]=v
 					end
 				end
 				for ix,v in IP(is) do
-					W()indent(n+1)
-					if math.fmod(ix,5)==0 then
+					Write_Newline()Indent(Indent_Level+1)
+					if Is_Normal and math.fmod(ix,5)==0 then
 						LE='--'..ix
 					end
-					R(v,n+1)W(CO)
+					Reference(v,Indent_Level+1)Write(Comma)
 					is[ix]=is
 				end
-				for k,v in Pairs(i) do
+				for k,v in Pairs(Input) do
 					if is[k]~=is then
-						W()indent(n+1)
-						if not (T(k)=='string' and (k:find'^[%a_][%w_]*$' or not Configure) and (W(k) or true)) then
-							W'['
+						Write_Newline()Indent(Indent_Level+1)
+						if not (Get_Type(k)=='string' and (not Configure or (not Keywords[k] and String_Find(k,'^[%a_][%w_]*$'))) and (Write(k) or true)) then
+							Write'['
 							Need_Break_Sequent_Open_Bracket=true
-							R(k,n+1)
-							W']'
+							Reference(k,Indent_Level+1)
+							Write']'
 						end
-						W'='R(v,n+1)W(CO)
+						Write'='Reference(v,Indent_Level+1)Write(Comma)
 					end
 				end
-				W()indent(n)W'}'
+				if Is_Compress then
+					TR(o)--remove last comma
+				end
+				Write_Newline()Indent(Indent_Level)Write'}'
 			end
+		else error("unhandle type: "..Type)
 		end
-		if id then
-			W'--[['W(tc[id])W']]'--reference cout
-			W')'
+		if ID then
+			if Is_Normal then
+				Write'--[['Write(tc[ID])Write']]'--reference cout
+			end
+			Write')'
 		end
 	end
-	R(Data,0)
+	Reference(Data,0)
 	
 	return Concat_Table(o)
 end--Data2String
+
 return Data2String
